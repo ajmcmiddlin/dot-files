45a2400291860e2336c9c23c2146b169be085dae
Andrew McCluskey <andrew.mccluskey@csiro.au>
2017-06-29 07:43:32 +1000

    Add comments to xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,9 +5,13 @@ import           XMonad.Hooks.ManageDocks         (avoidStruts, docks)
 
 import           System.Taffybar.Hooks.PagerHints (pagerHints)
 
+-- docks: add dock (panel) functionality to your configuration
+-- ewmh: https://en.wikipedia.org/wiki/Extended_Window_Manager_Hints - lets XMonad talk to panels
+-- pagerHints: add support for Taffybar's current layout and workspaces hints
 main :: IO ()
 main = xmonad . docks . ewmh . pagerHints $ defaultConfig
   {
     terminal = "uxrvt"
+    -- avoidStruts tells windows to avoid the "strut" where docks live
   , layoutHook = avoidStruts $ layoutHook defaultConfig
   }
