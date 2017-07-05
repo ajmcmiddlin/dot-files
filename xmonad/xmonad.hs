4195509e7d1011ac3b71b2adc68ae02321445ece
Andrew McCluskey <andrew.mccluskey@csiro.au>
2017-07-06 08:18:17 +1000

    Add WIP for custom key bindings in xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,23 @@
-import           XMonad                           (defaultConfig, xmonad)
-import           XMonad.Core                      (XConfig (..))
+import qualified Data.Map                         as M
+import           Data.Monoid                      ((<>))
+
+import qualified Graphics.X11.Types               as XT
+import           XMonad                           (Layout, X, XConfig (..),
+                                                   defaultConfig, spawn, xmonad,
+                                                   (.|.))
 import           XMonad.Hooks.EwmhDesktops        (ewmh)
 import           XMonad.Hooks.ManageDocks         (avoidStruts, docks,
                                                    manageDocks)
 import           XMonad.ManageHook                ((<+>))
 
 import           System.Taffybar.Hooks.PagerHints (pagerHints)
+
+myKeys :: XConfig Layout -> M.Map (XT.ButtonMask, XT.KeySym) (X ())
+myKeys conf@(XConfig {modMask = modm, keys = defaultKeys}) =
+  defaultKeys conf <> M.fromList
+    [ ((modm .|. XT.shiftMask, XT.xK_Return), spawn $ terminal conf)
+    , ((0, XT.xK_Print), spawn "maim -s ~/screenshots/$(date +%F_%T).png")
+    ]
 
 -- docks: add dock (panel) functionality to your configuration
 -- ewmh: https://en.wikipedia.org/wiki/Extended_Window_Manager_Hints - lets XMonad talk to panels
@@ -18,4 +30,5 @@ main = xmonad . docks . ewmh . pagerHints $ defaultConfig
   , layoutHook = avoidStruts $ layoutHook defaultConfig
     -- let XMonad manage docks (taffybar)
   , manageHook = manageDocks <+> manageHook defaultConfig
+--  , keys = myKeys
   }
