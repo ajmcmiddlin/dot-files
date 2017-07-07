5e8e5c96d94afd4b0b5715529eb6f08bbf3352b0
Andrew McCluskey <andrew@ajmccluskey.com>
2017-07-07 17:18:46 +1000

    Add volume, brightness, and screen layout keys

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -14,10 +14,22 @@ import           System.Taffybar.Hooks.PagerHints (pagerHints)
 
 myKeys :: XConfig Layout -> M.Map (XT.ButtonMask, XT.KeySym) (X ())
 myKeys conf@(XConfig {modMask = modm}) =
-  let kees =
+  let xK_X86MonBrightnessDown = 0x1008ff03
+      xK_X86MonBrightnessUp   = 0x1008ff02
+      xK_X86AudioLowerVolume  = 0x1008ff11
+      xK_X86AudioRaiseVolume  = 0x1008ff13
+      xK_X86AudioMute         = 0x1008ff12
+      kees =
         M.fromList [ ((0, XT.xK_Print), spawn "maim -c 1,0,0,0.6 -s ~/screenshots/$(date +%F_%T).png")
                    , ((modm, XT.xK_Print), spawn "maim -c 1,0,0,0.6 -s /dev/stdout | xclip -selection clipboard -t image/png -i")
                    , ((XT.controlMask .|. XT.mod1Mask, XT.xK_l), spawn "xscreensaver-command -lock")
+                   , ((0, xK_X86MonBrightnessDown), spawn "xbacklight -dec 5")
+                   , ((0, xK_X86MonBrightnessUp), spawn "xbacklight -inc 5")
+                   , ((0, xK_X86AudioLowerVolume), spawn "amixer sset Master 5%-")
+                   , ((0, xK_X86AudioRaiseVolume), spawn "amixer sset Master 5%+")
+                   , ((0, xK_X86AudioMute), spawn "amixer sset Master toggle")
+                   , ((XT.controlMask .|. XT.mod1Mask, XT.xK_q), spawn "~/.screenlayout/laptop-only.sh")
+                   , ((XT.controlMask .|. XT.mod1Mask, XT.xK_w), spawn "~/.screenlayout/the-fort.sh")
                    ]
   in kees <> keys def conf
 
