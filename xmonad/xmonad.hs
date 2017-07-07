618942122dcb5374985a56b68e67abb0dfa21887
Andrew McCluskey <andrew@ajmccluskey.com>
2017-07-07 11:49:31 +1000

    Add screenshot key bindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,7 +3,7 @@ import           Data.Monoid                      ((<>))
 
 import qualified Graphics.X11.Types               as XT
 import           XMonad                           (Layout, X, XConfig (..),
-                                                   defaultConfig, spawn, xmonad,
+                                                   def, spawn, xmonad,
                                                    (.|.))
 import           XMonad.Hooks.EwmhDesktops        (ewmh)
 import           XMonad.Hooks.ManageDocks         (avoidStruts, docks,
@@ -13,22 +13,23 @@ import           XMonad.ManageHook                ((<+>))
 import           System.Taffybar.Hooks.PagerHints (pagerHints)
 
 myKeys :: XConfig Layout -> M.Map (XT.ButtonMask, XT.KeySym) (X ())
-myKeys conf@(XConfig {modMask = modm, keys = defaultKeys}) =
-  defaultKeys conf <> M.fromList
-    [ ((modm .|. XT.shiftMask, XT.xK_Return), spawn $ terminal conf)
-    , ((0, XT.xK_Print), spawn "maim -s ~/screenshots/$(date +%F_%T).png")
-    ]
+myKeys conf@(XConfig {modMask = modm}) =
+  let kees =
+        M.fromList [ ((0, XT.xK_Print), spawn "maim -c 1,0,0,0.6 -s ~/screenshots/$(date +%F_%T).png")
+                   , ((modm, XT.xK_Print), spawn "maim -c 1,0,0,0.6 -s /dev/stdout | xclip -selection clipboard -t image/png -i")
+                   ]
+  in kees <> keys def conf
 
 -- docks: add dock (panel) functionality to your configuration
 -- ewmh: https://en.wikipedia.org/wiki/Extended_Window_Manager_Hints - lets XMonad talk to panels
 -- pagerHints: add support for Taffybar's current layout and workspaces hints
 main :: IO ()
-main = xmonad . docks . ewmh . pagerHints $ defaultConfig
+main = xmonad . docks . ewmh . pagerHints $ def
   {
     terminal = "urxvt"
     -- avoidStruts tells windows to avoid the "strut" where docks live
-  , layoutHook = avoidStruts $ layoutHook defaultConfig
+  , layoutHook = avoidStruts $ layoutHook def
     -- let XMonad manage docks (taffybar)
-  , manageHook = manageDocks <+> manageHook defaultConfig
---  , keys = myKeys
+  , manageHook = manageDocks <+> manageHook def
+  , keys = myKeys
   }
