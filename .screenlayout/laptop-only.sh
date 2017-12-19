a2c1f802249149e635203442086227fd87596a42
Andrew McCluskey <andrew@ajmccluskey.com>
2017-12-20 09:08:17 +1000

    Fix up xrandr scripts
    Had cases where displays had no modes.

--- .screenlayout/laptop-only.sh
+++ .screenlayout/laptop-only.sh
@@ -3,4 +3,4 @@ mode="laptopOnly"
 xrandr --rmmode $mode
 xrandr --newmode  $mode 197.97  2048 2184 2408 2768  1152 1153 1156 1192  -HSync +Vsync
 xrandr --addmode eDP1 laptopOnly
-xrandr --output VIRTUAL1 --off --output eDP1 --mode $mode --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode $mode --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off --output DP1-2 --off --output DP1-1 --off
