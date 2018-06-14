d306f33cd72038ea1b53fb70b4d3888051d3785e
Andrew McCluskey <andrew@ajmccluskey.com>
2018-06-15 08:20:05 +1000

    Add ssh-agent stuff to config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,1 +1,7 @@
 set -x EDITOR nvim
+
+source (dirname (status -f))"/init-ssh-agent.fish"
+if not ssh-add -l | grep /home/andrew/\.ssh/id_rsa > /dev/null
+  ssh-add
+end
+
---
+++ config/fish/functions/__ssh_agent_start.fish
@@ -0,0 +1,6 @@
+function __ssh_agent_start -d "start a new ssh agent"
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  chmod 600 $SSH_ENV
+  source $SSH_ENV > /dev/null
+  ssh-add
+end
