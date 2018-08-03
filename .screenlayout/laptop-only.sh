2b1e773363797add74a90acca9f32b6977b9edd4
Andrew McCluskey <andrew@ajmccluskey.com>
2018-08-03 21:44:29 +1000

    Set laptop-only mode based on hostname

--- .screenlayout/laptop-only.sh
+++ .screenlayout/laptop-only.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
+
 mode="laptopOnly"
+modeline=""
+
+host=$(hostname)
+case $(hostname) in
+  hermes)
+    modeline='172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync'
+    ;;
+  stevie)
+    modeline='197.97  2048 2184 2408 2768  1152 1153 1156 1192  -HSync +Vsync'
+    ;;
+  *)
+    echo "Unknown host: ${host}"
+    exit 1
+    ;;
+esac
+
 xrandr --rmmode $mode
-xrandr --newmode  $mode 197.97  2048 2184 2408 2768  1152 1153 1156 1192  -HSync +Vsync
+xrandr --newmode  $mode $modeline
 xrandr --addmode eDP1 laptopOnly
 xrandr --output VIRTUAL1 --off --output eDP1 --mode $mode --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off --output DP1-2 --off --output DP1-1 --off
