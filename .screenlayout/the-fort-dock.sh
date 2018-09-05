4d34edcb0b69a0e61b75e8b43597645c45f26ffd
Andrew McCluskey <andrew@ajmccluskey.com>
2018-09-05 14:19:00 +1000

    rm another unused layout script

--- .screenlayout/the-fort-dock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mode="1920x1200_59.95"
-xrandr --rmmode $mode
-xrandr --newmode  $mode 192.99  1920 2048 2256 2592  1200 1201 1204 1242  -HSync +Vsync
-xrandr --addmode DP1-2 $mode
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-1 --off --output DP1-2 --primary --mode $mode --pos 0x0 --rotate normal --output DP2 --off
-
