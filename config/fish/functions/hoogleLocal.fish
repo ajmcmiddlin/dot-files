f9c5b41d06804519e8e9d0d893f94641b06cb2c6
Andrew McMiddlin <andrew@ajmccluskey.com>
2018-12-05 08:43:07 +1000

    Take port argument in hoogleLocal function

--- config/fish/functions/hoogleLocal.fish
+++ config/fish/functions/hoogleLocal.fish
@@ -1,4 +1,9 @@
-# Defined in /tmp/fish.0ypEt9/hoogleLocal.fish @ line 1
 function hoogleLocal
-	nohup nix-shell --run 'hoogle server --local' &
+  if [ -z $argv ];
+      set hoogle_port 8080
+  else
+      set hoogle_port $argv[1]
+  end
+
+  nohup nix-shell --run "hoogle server --local --port=$hoogle_port &"
 end
